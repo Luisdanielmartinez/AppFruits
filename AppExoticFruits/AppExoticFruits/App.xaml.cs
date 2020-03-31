d8ef3ed6e4c9b6b361d387bdedf4b3784441d04a
Luis Daniel <Luis Daniel@DESKTOP-RK2RH1T>
2020-03-31 15:09:32 -0400

    initial commit

---
+++ AppExoticFruits/AppExoticFruits/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppExoticFruits.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppExoticFruits
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MarketPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
